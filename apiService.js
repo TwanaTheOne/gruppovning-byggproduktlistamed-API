95a0be939c5c98ddfe807f1852f56bf3a8f8982e
IngvaldssonMattias <mattias.ingvaldsson@chasacademy.se>
2025-11-13 10:46:54 +0100

    lagt in api med fetch i apiService.js+ länkat stylesheet

--- apiService.js
+++ apiService.js
@@ -1,8 +1,10 @@
+export async function fetchProducts() {
+  const response = await fetch("https://fakestoreapi.com/products");
 
-export async function fetchProducts(){
-    const response = await fetch("https://fakestoreapi.com/products")
-if(!response.ok)
-     throw new Error(console.log("You got an HTTP-error", response.status));
-    const data = await response.json();
-    return await fetchProducts()
+  if (!response.ok) {
+    throw new Error(`You got an HTTP-error: ${response.status}`);
+  }
+
+  const data = await response.json();
+  return data; 
 }
